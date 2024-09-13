483e2946f06165f512935867891b4afc2dd7a748
potato-lulw <om.patil17072@gmail.com>
2024-09-13 18:27:03 +0530

    Added Blog number to each Blog Card

--- app/components/blogcard.js
+++ app/components/blogcard.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
-const BlogCard = ({ title, imageSrc, comments, lastUpdated }) => {
+const BlogCard = ({ title, imageSrc, comments, lastUpdated, id }) => {
     const router = useRouter();
     const handleOnBlogCardClick = () => {
         // Navigate to the blog post page
@@ -13,9 +13,11 @@ const BlogCard = ({ title, imageSrc, comments, lastUpdated }) => {
         console.log(title)
     }
     return (
-        <div className="h-[160px]" onClick={handleOnBlogCardClick}>
+        <div className="h-[160px] relative" onClick={handleOnBlogCardClick}>
+            {id && <div className='absolute text-white h-6 top-2 left-2 bg-gray-900/50 rounded-md flex justify-center items-center px-2'>{id}</div>}
+
             <div className="h-[100px] from-myPink to-myPurple bg-gradient-to-r rounded-lg">
-                <Image src={imageSrc} alt={imageSrc} width={300} height={100} className="object-cover" />
+                {/* <Image src={imageSrc} alt={imageSrc} width={300} height={100} className="object-cover" /> */}
             </div>
             <h2 className="text-lg font-medium">Blog title: {title}</h2>
             <p className="text-sm dark:text-gray-200 text-gray-800">{comments} comments, {lastUpdated} mins ago</p>
